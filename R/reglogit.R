38b4322a4e9e9fe00e2fef539feca893eead623a
Robert B. Gramacy <rbgramacy@chicagobooth.edu>
2011-08-05 00:00:00 +0000

    version 1.0

---
+++ R/reglogit.R
@@ -0,0 +1,425 @@
+## functions for Gibbs sampling the parameters to the
+## regularized logit regression model
+
+## draw.beta:
+##
+## draw the regression coefficients conditional on the
+## latent zs, lambdas, and omegas, and the penalty
+## parameter nu, and thermo parameter kappa
+
+draw.beta <- function(X, z, lambda, omega, nu, sigma,
+                      kappa, kp, method)
+  {
+    ## negative nu and no sigma indicates L2 prior if sigma not null
+    if(nu < 0 && !is.null(sigma)) n <- 1
+    
+    ## covariance of the conditional
+    tXLi <- t(X) %*% diag(1/lambda)
+    if(nu > 0) {
+      Bi <- (kp/nu)^2 * 1/(sigma^2 * omega)
+      if(ncol(X) == length(sigma) + 1) Bi <- c(0, Bi)
+      Bi <- diag(Bi)
+    } else Bi <- 0
+    B <- solve(Bi + tXLi %*% X)
+
+    ## mean of the conditional
+    if(is.null(z)) { ## z=0 PDF representation
+
+      ## choices between different (a,b) parameterizations
+      if(method=="vaduva") { a <- 1; b <- kappa - 1 }
+      else { a  <- 0.5; b <- kappa - 0.5 }
+      kappa <- (a - 0.5*(a-b))
+
+      ## special handling in Binomial case
+      if(length(kappa == 1)) uk <- apply(X*kappa, 2, sum)
+      else  uk <- t(kappa) %*% X
+      b <- B %*% uk
+
+    } else ## CDF representation
+      b <- B %*% tXLi %*% (z - 0.5*(1-kappa)*lambda)
+
+    ## draw from the MVN
+    return(rmvnorm(1, b, B))
+  }
+
+
+## draw.nu:
+##
+## draw from the full conditional distribution of the
+## penalty parameter nu
+
+draw.nu <- function(beta, sigma, kp, nup=list(a=0, b=0))
+  {
+    ## intercept?
+    p <- length(sigma)
+    if(p == length(beta) - 1) beta <- beta[-1]
+
+    ## temper the prior
+    a <- kp*(nup$a+1)-1
+    b <- kp*nup$b
+
+    ## update using beta
+    a <- a + kp*p
+    ## a <- a + p
+    b <- b + kp*sum(abs(beta)/sigma)
+
+    ## return a sample from an inverse gamma
+    nu <- 1.0/rgamma(1, shape=a, scale=1/b)
+
+    ## return sampled nu
+    return(nu)
+  }
+
+
+## draw.z:
+##
+## draw the latent z-values given beta, y, and the
+## latent lambdas
+
+draw.z <- function(X, beta, lambda, kappa)
+{
+  n <- nrow(X)
+  xbeta <- X %*% beta
+  
+  return(.C("draw_z_R",
+            n = as.integer(n),
+            xbeta = as.double(xbeta),
+            beta = as.double(beta),
+            lambda = as.double(lambda),
+            kappa = as.double(kappa),
+            kl = as.integer(length(kappa)),
+            z = double(n),
+            PACKAGE = "reglogit")$z)
+}
+
+
+## draw.omega:
+##
+## draw the latent omega variables given the betas
+## and sigmas
+
+draw.omega <- function(beta, sigma, nu, kp)
+  {
+    if(nu < 0) stop("nu must be positive")
+
+    ## deal wth intercept in prior?
+    if(length(sigma) == length(beta) - 1) beta <- beta[-1]
+    m <- length(beta)
+    
+    return(.C("draw_omega_R",
+              m = as.integer(m),
+              beta = as.double(beta),
+              sigma = as.double(sigma),
+              nu = as.double(nu/kp),
+              omega = double(m),
+              PACKAGE = "reglogit")$omega)
+  }
+
+
+## draw.logit.prior
+##
+## testing function for the draw.lambda function
+## to make sure that the powered-up proposals from the
+## prior are working
+
+draw.logit.prior <- function(n, kappa, kmax=442)
+  {
+     k <- 0:kmax
+     lami <- 1 /(0.5*(1+k)*(kappa + k))
+     lambda <- draw.lambda.prior(n, lami)
+     x <- rnorm(length(lambda), sd=sqrt(lambda))
+     return(x)
+  }
+
+
+## draw.lambda:
+##
+## draw the latent lambda variables that help us
+## implement the logit link, via Metropolis-Hastings
+
+draw.lambda <- function(X, beta, lambda, kappa, kmax,
+                        zzero=TRUE, thin=kappa, method)
+  {
+    ## method ignored by the C-side if zzero = FALSE
+    if(method=="vaduva") method <- 1
+    else if(method=="slice") method <- 2
+    else method <- 3
+
+    ## calculate X * beta
+    xbeta <- X %*% beta
+    n <- length(xbeta)
+
+    ## call C code
+    return(.C("draw_lambda_R",
+              n = as.integer(n),
+              xbeta = as.double(xbeta),
+              kappa = as.double(kappa),
+              kl = as.integer(length(kappa)),
+              kmax = as.integer(kmax),
+              zzero = as.integer(zzero),
+              thin = as.integer(thin),
+              tl = as.integer(length(thin)),
+              method = as.integer(method),
+              lambda = as.double(lambda),
+              PACKAGE = "reglogit")$lambda)
+  }
+
+
+## draw.lambda.prior:
+##
+
+draw.lambda.prior <- function(n, psii)
+  {
+    return(.C("draw_lambda_prior_R",
+              n = as.integer(n),
+              psii = as.double(psii),
+              kmax = as.integer(length(psii)-1),
+              lambda = double(n),
+              PACKAGE = "reglogit")$lambda)
+  }
+
+
+
+## my.rinvgauss:
+##
+## draw from an Inverse Gaussian distribution following
+## Gentle p193
+
+my.rinvgauss <- function(n, mu, lambda)
+  {
+    return(.C("rinvgauss_R",
+              n = as.integer(n),
+              mu = as.double(mu),
+              lambda = as.double(lambda),
+              x = double(n),
+              PACKAGE = "reglogit")$x)
+  }
+
+
+## calc.lpost:
+##
+## calculate the log posterior probability of the
+## parameters in argument
+
+calc.lpost <- function(yX, beta, nu, kappa, kp, sigma,
+                       nup=list(a=0, b=0))
+  {
+    ## likelihood part
+    llik <- - sum(kappa*log(1 + exp(-drop(yX %*% beta))))
+
+    ## prior part
+    
+    ## deal with intercept in prior
+    p <- length(sigma)
+    if(p == length(beta) - 1) beta <- beta[-1]
+
+    ## for beta
+    if(nu > 0) lprior <- - kp*p*log(nu) - (kp/nu) * sum(abs(beta/sigma))
+    ## if(nu > 0) lprior <- - p*log(nu) - (kp/nu) * sum(abs(beta/sigma))
+    else if(!is.null(sigma)) lprior <- - sum((kp*beta/sigma)^2)
+    else lprior <- 0
+
+    ## inverse gamma prior for nu
+    if(!is.null(nup))
+      ## lprior <- lprior - kp*(2*(nup$a+1)*log(nu) + nup$b/(nu^2))
+      lprior <- lprior - kp*((nup$a+1)*log(nu) + nup$b/nu)
+
+    ## return log posterior
+    return(llik + lprior)
+  }
+
+
+## preprocess:
+##
+## pre-process the X and y and kappa data depending on
+## how binomial responses might be processed
+
+preprocess <- function(X, y, N, flatten, kappa)
+  {
+    ## process y's and set up design matrices
+    if(is.null(N) || flatten == TRUE) { 
+      
+      if(flatten) { ## flattened binomial case
+        if(is.null(N)) stop("flatten only applies for N != NULL")
+        ye <- NULL
+        Xe <- NULL
+        for(i in 1:length(N)) {
+          Xe <- rbind(Xe, matrix(rep(X[i,], N[i]), nrow=N[i], byrow=TRUE))
+          ye <- c(ye, rep(1, y[i]), rep(0, N[i]-y[i]))
+        }
+        y <- ye; X <- Xe;
+      }
+
+      ## create y in {-1,+1}
+      ypm1 <- y
+      ypm1[y == 0] <- -1
+      
+      ## create y *. X
+      yX <- diag(ypm1) %*% X
+      
+    } else {  ## unflattened binomial case
+      
+      ## sanity checks
+      if(length(N) != length(y))
+        stop("should have length(N) = length(y)")
+      if(any(N < y)) stop("should have all N >= y")
+      
+      ## construct yX and re-use kappa
+      n <- length(y)
+      kappa <- kappa*c(y, N-y)
+      knz <- kappa != 0
+      yX <- rbind(X, -X)[knz,]
+      y <- c(rep(1,n),rep(0,n))[knz]
+      kappa <- kappa[knz]
+      n <- length(kappa)
+    }
+
+    ## return the data we're going to work with
+    return(list(yX=yX, y=y, kappa=kappa))
+  }
+
+
+
+## reglogit:
+##
+## function for Gibbs sampling from the a logistic
+## regression model, sigma=NULL and nu < 0 indicates
+## no prior
+
+reglogit <- function(T, y, X, N=NULL, flatten=FALSE, sigma=1, nu=1,
+                  kappa=1, icept=TRUE, normalize=TRUE,
+                  zzero=TRUE, powerprior=TRUE, kmax=442,
+                  bstart=NULL, lt=NULL, nup=list(a=2, b=0.1),
+                  method=c("MH", "slice", "vaduva"), verb=100)
+{
+  ## getting and checking data size
+  m <- ncol(X)
+  n <- length(y)
+  if(n != nrow(X)) stop("dimension mismatch")
+
+  ## check the method argument
+  method <- match.arg(method)
+  
+  ## design matrix processing
+  X <- as.matrix(X)
+  if(normalize) {
+    one <- rep(1, n)
+    normx <- sqrt(drop(one %*% (X^2)))
+    if(any(normx == 0)) stop("degenerate X cols")
+    X <- as.matrix(as.data.frame(scale(X, FALSE, normx)))
+  } else normx <- rep(1, ncol(X))
+
+  ## init sigma
+  if(length(sigma) == 1) sigma <- rep(sigma, ncol(X))
+
+  ## add on intecept term?
+  if(icept) {
+    X <- cbind(1, X)
+    normx <- c(1, normx)
+  }
+
+  ## put the starting beta value on the right scale
+  if(!is.null(bstart) && normalize) bstart <- bstart*normx
+  else if(is.null(bstart)) bstart <- rnorm(m+icept)
+
+  ## allocate beta, and set starting position
+  beta <- matrix(NA, nrow=T, ncol=m+icept)
+  beta[1,] <- bstart
+  map <- list(beta=bstart)
+  
+  ## check if we are inferring nu
+  if(!is.null(nup)) {
+    nus <- rep(NA, T)
+    if(nu <= 0) stop("starting nu must be positive")
+    nus[1] <- nu
+  } else nus <- NULL
+  
+  ## check for agreement between nu and sigma
+  if(is.null(sigma) && nu > 0)
+    stop("must define sigma for regularization")
+
+  ## initial values of the regularization latent variables
+  if(nu > 0) {  ## omega lasso/L1 latents
+    omega <- matrix(NA, nrow=T, ncol=m)
+    ot <- omega[1,] <- 1
+    omega[,1] <- 1
+  } else { omega <- NULL; ot <- rep(1, m) }
+
+  ## save the original kappa
+  if(powerprior) kp <- kappa
+  else kp <- 1
+
+  ## process y's and set up design matrices
+  nd <- preprocess(X, y, N, flatten, kappa)
+  yX <- nd$yX; y <- nd$y; kappa <- nd$kappa
+  n <- nrow(yX)
+ 
+  ## initialize lambda latent variables
+  lambda <- matrix(NA, nrow=T, ncol=n)
+  if(is.null(lt)) lt <- rep(1, n)
+  map$lambda <- lambda[1,] <- lt
+
+  ## initial values for the logit latents
+  if(!zzero) {
+    z <- matrix(NA, nrow=T, ncol=n)
+    z[1,] <- zt <- y
+  } else { z <- zt <- NULL }
+
+  ## allocate and initial log posterior calculation
+  lpost <- rep(NA, T)
+  map$lpost <- lpost[1] <-
+    calc.lpost(yX, map$beta, nu, kappa, kp, sigma, nup)
+  if(!is.null(nus)) map$nu <- nu
+  
+  ## Gibbs sampling rounds
+  for(t in 2:T) {
+
+    ## progress meter
+    if(t %% verb == 0) cat("round", t, "\n")
+    
+    ## if regularizing, draw the latent omega values
+    if(nu > 0) ot <- omega[t,] <- draw.omega(beta[t-1,], sigma, nu, kp)
+
+    ## if logistic, draw the latent lambda values
+    lt <- lambda[t,] <-
+      draw.lambda(yX, beta[t-1,], lt, kappa, kmax, zzero, method=method)
+
+    ## draw the latent z values
+    if(!zzero) zt <- z[t,] <- draw.z(yX, beta[t-1,], lt, kappa)
+
+    ## draw the regression coefficients
+    beta[t,] <- draw.beta(yX, zt, lt, ot, nu, sigma, kappa, kp, method=method)
+
+    ## maybe draw samples from nu
+    if(!is.null(nus)) nu <- nus[t] <- draw.nu(beta[t,], sigma, kp, nup)
+    
+    ## calculate the posterior probability to find the map
+    lpost[t] <- calc.lpost(yX, beta[t,], nu, kappa, kp, sigma, nup)
+
+    ## update the map
+    if(lpost[t] > map$lpost) {
+      map <- list(beta=beta[t,], lpost=lpost[t], lambda=lambda[t,])
+      if(!is.null(nus)) map$nu <- nus[t]
+    }
+      
+  }
+
+  ## un-normalize
+  if(normalize) {
+    beta <- as.matrix(as.data.frame(scale(beta, FALSE, scale=normx)))
+    map$beta <- map$beta/normx
+  }     
+
+  ## construct the return object
+  r <- list(X=X, y=y, beta=beta, lambda=lambda, lpost=lpost,
+            map=map, kappa=kappa)
+  if(nu > 0) r$omega <- omega
+  if(normalize) r$normx <- normx
+  if(!zzero) r$z <- z
+  if(!is.null(nus)) r$nu <- nus
+
+  ## assign a class to the object
+  class(r) <- "reglogit"
+  
+  return(r)
+}
---
+++ src/gibbs.c
@@ -0,0 +1,468 @@
+// #include "rhelp.h"
+#include <stdlib.h>
+#include <assert.h>
+#include <R.h>
+#include <Rmath.h>
+
+#define TOL sqrt(DOUBLE_EPS)
+
+/*
+ * sq:
+ * 
+ * calculate the square of x
+ */
+
+double sq(double x)
+{
+  return x*x;
+}
+
+
+/*
+ * rinvgauss:
+ *
+ * Michael/Schucany/Haas Method for generating Inverse Gaussian
+ * random variable, as given in Gentle's book on page 193
+ */
+
+double rinvgauss(const double mu, const double lambda)
+{
+  double u, y, x1, mu2, l2;
+
+  y = sq(norm_rand());
+  mu2 = sq(mu);
+  l2 = 2*lambda;
+  x1 = mu + mu2*y/l2 - (mu/l2)* sqrt(4*mu*lambda*y + mu2*sq(y));
+
+  u = unif_rand();
+  if(u <= mu/(mu + x1)) return x1;
+  else return mu2/x1;
+}
+
+
+/*
+ * rtnorm_reject:
+ *
+ * dummy function in place of the Robert (1995) algorithm 
+ * based on proposals from the exponential, should have that 
+ * mean < tau 
+ */
+
+double rtnorm_reject(double mean, double tau, double sd)
+{
+  double x, z, lambda;
+  int cnt;
+
+  /* Christian Robert's way */
+  assert(mean < tau);
+  tau = (tau - mean)/sd;
+
+  /* optimal exponential rate parameter */
+  lambda = 0.5*(tau + sqrt(sq(tau) + 4.0));
+
+  /* do the rejection sampling */
+  cnt = 0;
+  do {
+    z = rexp(1.0/lambda) + tau;
+  } while (unif_rand() > exp(0.0-0.5*sq(z - lambda)));
+
+  /* put x back on the right scale */
+  x = z*sd + mean;
+
+  assert(x > 0);
+  return(x);
+
+}
+
+
+/*
+ * draw_lambda_prior:
+ *
+ * draw Lambda from its (infinite mixture) prior
+ */
+
+double draw_lambda_prior(double *psii, int kmax)
+{
+  double lambda;
+  int k;
+
+  lambda = 0.0;
+  for(k=0; k<=kmax; k++) {
+    lambda += psii[k] * rexp(1.0);
+  }
+  
+  return lambda;
+}
+
+
+/* 
+ * draw_lambda_prior_R:
+ *
+ * R interface to lambda draw from the posterior conditional
+ * by rejection sampling and proposals from the prior
+ */
+
+void draw_lambda_prior_R(int *n_in, double *psii_in, int *kmax_in, 
+			 double *lambda_out)
+{
+  int i;
+
+  GetRNGstate();
+
+  for(i=0; i<*n_in; i++) 
+    lambda_out[i] = draw_lambda_prior(psii_in, *kmax_in);
+
+  PutRNGstate();  
+}
+
+
+/*
+ * draw_lambda:
+ *
+ * Metropolis-Hastings algorithm for drawing lambda from its 
+ * full conditional -- uses proposals from the prior
+ */
+
+double draw_lambda(double lambda_old, double xbeta, 
+		   double kappa, int kmax, int thin)
+{
+  int t, k;
+  double lambda, lp, lpold, m, s;
+  double *psii;
+
+  /* calculate the probability og the previous lambda */
+  s = sqrt(lambda_old);
+  m = xbeta + 0.5*(1.0-kappa)*lambda_old;
+  lpold = pnorm(0.0, m, s, 0, 1);
+
+  /* allocate psii */
+  psii = (double*) malloc(sizeof(double) * (kmax+1));
+  for(k=0; k<=kmax; k++) psii[k] =  2.0/((1.0+k)*(kappa+k));
+    
+  /* thinning is essential when kappa is large */
+  for(t=0; t<thin; t++) {
+
+    /* propose a new lambda from the prior */
+    lambda = draw_lambda_prior(psii, kmax);
+    
+    /* calculate the probability of the propsed lambda */
+    s = sqrt(lambda);
+    m = xbeta + 0.5*(1.0-kappa)*lambda;
+    lp = pnorm(0.0, m, s, 0, 1);
+    
+    /* MH accept or reject */
+    if(unif_rand() < exp(lp - lpold)) {
+      lambda_old = lambda;
+      lpold = lp;
+    }
+  }
+
+  /* possibly clean up psii */
+  free(psii);
+
+  return lambda_old;
+}
+
+
+
+/*
+ * draw_lambda_zz_MH:
+ *
+ * Metropolis-Hastings algorithm for drawing lambda from its 
+ * full conditional under the z=0 model -- uses proposals 
+ * from the prior
+ */
+
+double draw_lambda_zz_MH(double lambda_old, double xbeta, 
+			       double kappa, int kmax, int thin)
+{
+  int t, k;
+  double lambda, lp, lpold, m, s;
+  double *psii;
+
+  /* calculate the probability og the previous lambda */
+  s = sqrt(lambda_old);
+  m = xbeta + 0.5*(1.0-kappa)*lambda_old;
+  lpold = dnorm(0.0, m, s, 1);
+
+  /* allocate psii */
+  psii = (double*) malloc(sizeof(double) * (kmax+1));
+  for(k=0; k<=kmax; k++) psii[k] = 2.0/((0.5+k)*(kappa-0.5+k));
+    
+  /* thinning is essential when kappa is large */
+  for(t=0; t<thin; t++) {
+
+    /* propose a new lambda from the prior */
+    lambda = draw_lambda_prior(psii, kmax);
+    
+    /* calculate the probability of the propsed lambda */
+    s = sqrt(lambda);
+    m = xbeta + 0.5*(1.0-kappa)*lambda;
+    lp = dnorm(0.0, m, s, 1);
+
+    /* MH accept or reject */
+    if(unif_rand() < exp(lp - lpold)) {
+      lambda_old = lambda;
+      lpold = lp;
+    }
+  }
+
+  /* possibly clean up psii */
+  free(psii);
+
+  return lambda_old;
+}
+
+
+/*
+ * draw_lambda_zz_slice:
+ *
+ * Metropolis-Hastings algorithm for drawing lambda from its 
+ * full conditional under the z=0 model -- uses slice sampling
+ */
+
+double draw_lambda_zz_slice(double lambda_old, double xbeta, 
+			       double kappa, int kmax)
+{
+  int k, cnt;
+  double lambda, p, pold, m, s, u;
+  double *psii;
+
+  /* calculate the probability og the previous lambda */
+  s = sqrt(lambda_old);
+  m = xbeta + 0.5*(1.0-kappa)*lambda_old;
+  pold = dnorm(0.0, m, s, 0);
+
+  /* draw the uniform latent variable */
+  u = runif(0, pold);
+
+  /* allocate psii */
+  psii = (double*) malloc(sizeof(double) * (kmax+1));
+  for(k=0; k<=kmax; k++) psii[k] = 2.0/((0.5+k)*(kappa-0.5+k));
+  
+  cnt = 0;
+  do {
+
+    /* propose a new lambda from the prior */
+    lambda = draw_lambda_prior(psii, kmax);
+    
+    /* calculate the probability of the propsed lambda */
+    s = sqrt(lambda);
+    m = xbeta + 0.5*(1.0-kappa)*lambda;
+    p = dnorm(0.0, m, s, 0);
+
+    /* sanity print for poor rejection rate */
+    cnt++;
+    if(cnt % 1000000 == 0) 
+      fprintf(stderr, "slice=%d, xbeta=%g, lambda=%g, u=%g, kappa=%g\n", 
+	      cnt, xbeta, lambda_old, u, kappa);
+
+  } while(p <= u);
+
+
+  /* clean up */
+  free(psii);
+
+  return lambda;
+}
+
+
+/*
+ * draw_lambda_zz_vaduva:
+ *
+ * Vaduva's generalization of the rejection method for
+ * sampling lambda_i in the z=0 version of the model
+ */
+
+double draw_lambda_zz_vaduva(double xbeta, double kappa, int kmax)
+{
+  int k, cnt;
+  double lambda, INv, INm, y;
+  double *psii;
+
+  /* lambda parmater for IN */
+  INv = 0.25*(2.0-kappa)*(2.0-kappa) + kappa - 1.0;
+  INm = sqrt(INv)/fabs(xbeta);
+
+  /* allocate psii */
+  psii = (double*) malloc(sizeof(double) * (kmax+1));
+  for(k=0; k<=kmax; k++) psii[k] = 2.0/((1.0+k)*(kappa+1.0+k));
+  
+  cnt = 0;
+  do {
+    
+    /* x draw in the notes */
+    lambda = 1.0/rinvgauss(INm, INv);
+
+    /* y draw in the notes */
+    y = draw_lambda_prior(psii, kmax);
+
+    /* sanity print for poor rejection rate */
+    cnt++;
+    if(cnt % 100000 == 0) 
+      fprintf(stderr, "vaduva=%d, x=%g, y=%g, xbeta=%g, kappa=%g, INv=%g\n",
+	      cnt, lambda, y, xbeta, kappa, INv);
+
+  } while(lambda < y);
+
+  
+  /* clean up */
+  free(psii);
+
+  return lambda;
+}
+
+
+/* 
+ * draw_lambda_R:
+ *
+ * R interface to lambda draw from the posterior conditional
+ * by rejection sampling and proposals from the prior
+ */
+
+void draw_lambda_R(int *n_in, double *xbeta_in, double *kappa_in, 
+		   int *kl_in, int *kmax_in, int *zzero_in, 
+		   int *thin_in, int *tl_in, int *method_in, 
+		   double *lambda_inout)
+{
+  int i, thin;
+  double kappa;
+
+  GetRNGstate();
+
+  /* initial values */
+  kappa = *kappa_in;
+  thin = *thin_in;
+
+  for(i=0; i<*n_in; i++) {
+
+    /* depends on whether we are vectorizing or not */
+    if(*kl_in > 1) kappa = kappa_in[i];
+    if(*tl_in > 1) thin = thin_in[i];
+
+    /* the actual draw */
+    if(!*zzero_in)
+      lambda_inout[i] = draw_lambda(lambda_inout[i], xbeta_in[i], 
+				    kappa, *kmax_in, thin);
+    else {
+      switch(*method_in) {
+      case 1: 
+	lambda_inout[i] = draw_lambda_zz_vaduva(xbeta_in[i], kappa, 
+						*kmax_in); 
+	break;
+      case 2:
+        lambda_inout[i] = 
+	  draw_lambda_zz_slice(lambda_inout[i], xbeta_in[i], kappa, 
+			       *kmax_in);
+	break;
+      case 3:
+	lambda_inout[i] = 
+	  draw_lambda_zz_MH(lambda_inout[i], xbeta_in[i], kappa, 
+			    *kmax_in, thin);
+	break;
+      default: error("no default defined");
+      }
+    }
+  }
+
+  PutRNGstate();
+}
+
+
+/* 
+ * draw_z_R:
+ *
+ * C-side for R function draw.z, which draws from the full
+ * conditional of the latent z-values given y, beta, and 
+ * the latent lambdas
+ */
+
+void draw_z_R(int *n_in, double *xbeta_in, double *beta_in, 
+	      double *lambda_in, double *kappa_in, int *kl_in, 
+	      double *z_out)
+{
+  int n, i;
+  double xbeta, madj, lambda_sqrt;
+
+  /* copy scalars */
+  n = *n_in;
+ 
+  /* calculate mean multiplicative adjustment due to kappa */
+  madj = 0.5*(1.0 - *kappa_in);
+
+  /* get the RNG state */
+  GetRNGstate();
+
+  /* loop over rows of X */
+  for(i=0; i<n; i++) {
+
+    /* depends on whether we are vectorizing or not */
+    if(*kl_in > 1) madj = 0.5*(1.0 - kappa_in[i]);
+    
+    /* calculate the mean and variance of the normal */
+    xbeta = xbeta_in[i] + madj * lambda_in[i];
+    lambda_sqrt = sqrt(lambda_in[i]);
+
+    /* draw until we get one in the right half-plane */
+    if(xbeta >= 0) {
+      do { z_out[i] = rnorm(xbeta, lambda_sqrt);
+      } while (z_out[i] < 0.0);
+    } else { 
+      z_out[i] = rtnorm_reject(xbeta, 0.0, lambda_sqrt);
+      assert(z_out[i] > 0.0);
+    }
+  }
+
+  /* put the RNG state back */
+  PutRNGstate();
+}
+
+
+/* 
+ * draw_omega_R:
+ *
+ * C-side for R function draw.omega, which draws from the full
+ * conditional of the latent omega-values given beta, 
+ * sigma, and nu
+ */
+
+void draw_omega_R(int *m_in, double *beta_in, double *sigma_in, 
+		  double *nu_in, double *omega_out)
+{
+  int j;
+  double mu;
+
+  GetRNGstate();
+
+  /* for each latent omega */
+  for(j=0; j<*m_in; j++) {
+    
+    /* calculate the mean of the inverse gaussian */
+    mu = *nu_in * sigma_in[j] / fabs(beta_in[j]);
+
+    /* sample from the inverse gaussian and invert */
+    omega_out[j] = 1.0/rinvgauss(mu, 1.0);
+  }
+
+  PutRNGstate();
+}
+
+
+/*
+ * rinvgauss_R:
+ *
+ * R interface to R_invgauss
+ */
+
+void rinvgauss_R(int* n_in, double *mu_in, double *lambda_in, 
+		 double *x_out)
+{
+  int i;
+
+  GetRNGstate();
+
+  for(i=0; i<*n_in; i++) 
+    x_out[i] = rinvgauss(*mu_in, *lambda_in);
+
+  PutRNGstate();
+}
